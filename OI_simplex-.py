aac2a80f1d264ca15b8e4b2b4440a4d6df5d1696
Hugo Fdz <hugofdzgarcia@gmail.com>
2025-09-30 21:27:30 +0000

    MetodoSimplex

--- OI_simplex-.py
+++ OI_simplex-.py
@@ -1,1 +1,5 @@
 from pulp import *
+
+#variables automaticas
+number_variables = int(input("Cuantas variables vas a ocupar: "))
+variables = [LpVariable(f"x{i + 1}",lowBount = 0) for i in range(number_variables)]
