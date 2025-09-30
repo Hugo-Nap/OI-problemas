9f4a1ed56f175e6f5d6918687ac53a2ddec0d946
gereher051219-gif <gereher121905@gmail.com>
2025-09-30 23:25:21 +0000

    Metodo Simplex

--- OI_simplex-.py
+++ OI_simplex-.py
@@ -1,5 +1,13 @@
 from pulp import *
 
+#pedirle al usuario si quiere maximizar, presione tecla 1 o minimizar presoina tecla 2
+min_max =int(input("Si quiere maximizar presione tecla 1 si quiere minimizar presione tecla 2"))
+
+if(min_max == 1):
+    problem = LpProblem("Metodo Simplex " , LpMaximize)
+    else:
+        LpMinimize("Metodo Simplex" , LpMinimize)
+
 #variables automaticas
 number_variables = int(input("Cuantas variables vas a ocupar: "))
 variables = [LpVariable(f"x{i + 1}",lowBount = 0) for i in range(number_variables)]
