065a21cadb5bc0069eac0621d88fd8b14e71bfbf
Hugo Fdz <hugofdzgarcia@gmail.com>
2025-10-01 03:58:24 +0000

    Metodo Simplex

--- OI_simplex-.py
+++ OI_simplex-.py
@@ -11,3 +11,10 @@ if(min_max == 1):
 #variables automaticas
 number_variables = int(input("Cuantas variables vas a ocupar: "))
 variables = [LpVariable(f"x{i + 1}",lowBount = 0) for i in range(number_variables)]
+funtion_objective = 0
+
+# ingreso de la funcion onjetivo
+for i in range(number_variables):
+    z_coefficients = float(input("ingrese el coeficiente de la variable x {i + 1}"))
+    # mostrar variables con su coeficiente
+    print(f"Coeficiente de x{i + 1} : {z_coefficients}")
