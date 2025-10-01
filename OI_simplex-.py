fa402129309af63f436c36edc4107ff1967bd62b
gereher051219-gif <gereher121905@gmail.com>
2025-10-01 04:24:17 +0000

    Metodo Simplex

--- OI_simplex-.py
+++ OI_simplex-.py
@@ -5,8 +5,10 @@ min_max =int(input("Si quiere maximizar presione tecla 1 si quiere minimizar pre
 
 if(min_max == 1):
     problem = LpProblem("Metodo Simplex " , LpMaximize)
-    else:
-        LpMinimize("Metodo Simplex" , LpMinimize)
+    else if(min_max == 2):
+        problem = LpProblem("Metodo Simplex" , LpMinimize)
+    else: 
+        print ("Dato invalido, intentelo de nuevo..")
 
 #variables automaticas
 number_variables = int(input("Cuantas variables vas a ocupar: "))
@@ -17,5 +19,8 @@ funtion_objective = 0
 for i in range(number_variables):
     z_coefficients = float(input("ingrese el coeficiente de la variable x {i + 1}"))
     # mostrar variables con su coeficiente
-    print(f"Coeficiente de x{i + 1} : {z_coefficients}")
+    print(f"Coeficiente de x1: {i + 1} : {z_coefficients}")
     funtion_objective += z_coefficients * variables[i]
+
+#resolver problema
+    
