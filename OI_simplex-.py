bcbc5aa208092eb7b3a7c922fc6bd347af904652
Hugo Fdz <hugofdzgarcia@gmail.com>
2025-10-02 03:50:41 +0000

    Metodo Simplex

--- OI_simplex-.py
+++ OI_simplex-.py
@@ -5,19 +5,19 @@ min_max =int(input("Si quiere maximizar presione tecla 1 si quiere minimizar pre
 
 if(min_max == 1):
     problem = LpProblem("Metodo Simplex " , LpMaximize)
-    else if(min_max == 2):
-        problem = LpProblem("Metodo Simplex" , LpMinimize)
-    else: 
-        print ("Dato invalido, intentelo de nuevo..")
+elif(min_max == 2):
+    problem = LpProblem("Metodo Simplex" , LpMinimize)
+else: 
+    print ("Dato invalido, intentelo de nuevo..")
 
 #variables automaticas
 number_variables = int(input("Cuantas variables vas a ocupar: "))
-variables = [LpVariable(f"x{i + 1}",lowBount = 0) for i in range(number_variables)]
+variables = [LpVariable(f"x{i + 1}",lowBound = 0) for i in range(number_variables)]
 funtion_objective = 0
 
-# ingreso de la funcion onjetivo
+# ingreso de la funcion objetivo
 for i in range(number_variables):
-    z_coefficients = float(input("ingrese el coeficiente de la variable x {i + 1}"))
+    z_coefficients = float(input(f"ingrese el coeficiente de la variable x {i + 1}"))
     # mostrar variables con su coeficiente
     print(f"Coeficiente de x1: {i + 1} : {z_coefficients}")
     funtion_objective += z_coefficients * variables[i]
@@ -28,12 +28,17 @@ number_restriction = int(input("cuantas restricciones va a ingresarse "))
 for i in range(int (number_restriction))
     coefficients = []#lista vacia llamada coeficientes
     for j in range(int (number_variables))
-    coeff = float(input("ingrese el coeficiente de la variable x {j + 1} en la restriccion {i + 1}"))
+    coeff = float(input(f"ingrese el coeficiente de la variable x {j + 1} en la restriccion {i + 1}: "))
     print(f"coeficiente de x {j + 1} en la restriccion {i + 1} : {coeff}") 
     coefficients.append(coeff)#agrega el valor al final de la lista
-restrition = sum(coefficients[j] *  variable[j] for j in range (int(number_variables)))
-lim = float(input("ingresa el valor limite de la restriccion {i + 1}"))
-problem = (restriccion <= lim)#acomodar los ejercicios de acuerdo al ejercicio
+restrition = sum(coefficients[j] *  variables[j] for j in range (int(number_variables)))
+lim = float(input(f"ingresa el valor limite de la restriccion {i + 1}": ))
+problem = (restrition <= lim)#acomodar las resticciones de acuerdo al ejercicio
 
 #resolver problema
-    
+problem.solve()
+print("Estado: ", LpStatus[problem.status])
+print("Valor de la funcion objetivo: ", value(problem.objective))
+print("Valores de las variables: ")
+for v in problem.variables():
+    print(f"{v.name} = {v.varValue}")
