14bffc459f4e696274ac936a64797c9e6226f37e
gereher051219-gif <gereher121905@gmail.com>
2025-10-02 03:15:33 +0000

    Metodo Simplex

--- OI_simplex-.py
+++ OI_simplex-.py
@@ -21,6 +21,19 @@ for i in range(number_variables):
     # mostrar variables con su coeficiente
     print(f"Coeficiente de x1: {i + 1} : {z_coefficients}")
     funtion_objective += z_coefficients * variables[i]
+problem = funtion_objective + problem
+
+#restricciones
+number_restriction = int(input("cuantas restricciones va a ingresarse "))
+for i in range(int (number_restriction))
+    coefficients = []#lista vacia llamada coeficientes
+    for j in range(int (number_variables))
+    coeff = float(input("ingrese el coeficiente de la variable x {j + 1} en la restriccion {i + 1}"))
+    print(f"coeficiente de x {j + 1} en la restriccion {i + 1} : {coeff}") 
+    coefficients.append(coeff)#agrega el valor al final de la lista
+restrition = sum(coefficients[j] *  variable[j] for j in range (int(number_variables)))
+lim = float(input("ingresa el valor limite de la restriccion {i + 1}"))
+problem = (restriccion <= lim)#acomodar los ejercicios de acuerdo al ejercicio
 
 #resolver problema
     
